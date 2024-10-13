ebd18756a3c5d009591141da5e1772834829bc69
manzitresor <manzitre@gmail.com>
2024-10-13 12:59:25 +0200

    chore: add GET/articles endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ArticlesModule],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Controller('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  create() {
+    return this.articlesService.create();
+  }
+
+  @Get()
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.articlesService.remove(+id);
+  }
+}
