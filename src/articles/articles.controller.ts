c99f7eca730d7df6396090d4be5344fbe1cfc57c
manzitresor <manzitre@gmail.com>
2024-10-13 14:29:40 +0200

    chore: add swagger documentation description

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -10,37 +10,46 @@ import {
 import { ArticlesService } from './articles.service';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { CreateArticleDto } from './dto/create-article.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './articles.entity';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleData: CreateArticleDto) {
     return this.articlesService.create(createArticleData);
   }
 
   @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articlesService.findAll();
   }
 
   @Get('drafts')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findDrafts() {
     return this.articlesService.findDrafts();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   findOne(@Param('id') id: string) {
     return this.articlesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   remove(@Param('id') id: string) {
     return this.articlesService.remove(+id);
   }
